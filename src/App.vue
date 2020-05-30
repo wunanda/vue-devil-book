ee35a7f12ea92a5847cfcc23e2f1f56ec004702f
wunan <395909614@qq.com>
2020-05-30 16:19:55 +0800

    '引入web字体'

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <span class="text">ABCDEFG</span>
 <!--    <div id="nav">-->
 <!--      <router-link to="/">Home</router-link> |-->
 <!--      <router-link to="/about">About</router-link>-->
@@ -8,6 +9,10 @@
   </div>
 </template>
 
-<style>
-
+<style scoped>
+  .text {
+    font-family: 'Days One';
+    font-size: 20px;
+    color:orange;
+  }
 </style>

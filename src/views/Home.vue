c10efbc5196e5751c9f085ba808c974d599baf1a
wunan <395909614@qq.com>
2020-05-30 15:57:54 +0800

    'svg图标引用和npmsass-loader、npde-sass'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,22 +1,20 @@
 <template>
   <div class="home">
-    <div id="read"></div>
+    <span class="icon-bookmark"></span>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Epub from 'epubjs'
-global.ePub = Epub
+
 export default {
-  name: 'home',
-  mounted () {
-    this.book = new Epub('/2017_Book_InterdisciplinaryPerspectivesO.epub')
-    console.log(this.book)
-    this.book.renderTo('read', {
-      width: window.innerWidth,
-      height: window.innerHeight
-    }).display()
-  }
+  name: 'home'
 }
 </script>
+
+<style scoped>
+.icon-bookmark{
+  font-size: 20px;
+  color: red;
+}
+</style>

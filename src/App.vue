e47503a2d5ec0ff8c4a055e29df71da13f43b45c
wunan <395909614@qq.com>
2020-05-31 07:22:32 +0800

    '手指翻页Ok'

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,12 @@
 
 <style lang="scss" scoped>
   #app {
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    z-index: 0;
     width: 100%;
     height: 100%;
     overflow: hidden;
--- src/components/ebook/EbookReader.vue
+++ src/components/ebook/EbookReader.vue
@@ -16,13 +16,46 @@
       initEpub () {
         const url = 'http://192.168.0.111:8081/epub/' + this.fileName + '.epub'
         this.book = new Epub(url)
-        console.log(this.book)
         this.redintion = this.book.renderTo('read', {
-          width: innerHeight,
-          height: innerHeight
+          width: window.innerWidth,
+          height: window.innerHeight
           // method: 'default' // 微信兼容性 但是加上的话web解析不出带图片的
         })
         this.redintion.display()
+        // 手指触摸事件
+        this.redintion.on('touchstart', event => {
+          this.touchStartX = event.changedTouches[0].clientX
+          this.touchStartTime = event.timeStamp
+        })
+        // 离开手指
+        this.redintion.on('touchend', event => {
+          const offsetX = event.changedTouches[0].clientX - this.touchStartX
+          const time = event.timeStamp - this.touchStartTime
+          if (time < 500 && offsetX > 40) {
+            this.prevPage()
+          } else if (time < 500 && offsetX < -40) {
+            this.nextPage()
+          } else {
+            this.toggleTitleAndMenu()
+          }
+          // event.preventDefault()
+          event.stopPropagation()
+        })
+      },
+      prevPage () {
+        if (this.redintion) {
+          this.redintion.prev()
+        }
+      },
+      nextPage () {
+        if (this.redintion) {
+          this.redintion.next()
+        }
+      },
+      toggleTitleAndMenu () {
+        if (this.redintion) {
+
+        }
       }
     },
     mounted () {
